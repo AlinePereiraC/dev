24538986368a4047c8ccdf8c5bd4be0f910c47d6
AlinePereiraC <126829018+AlinePereiraC@users.noreply.github.com>
2023-03-02 20:03:57 -0300

    alineatividade

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+ const a = Number(prompt("Digite o valor do seu salario: "))
+
+ const b = Number(prompt("Digite a porcentagem do aumento: ")) 
+
+ resultado = a*((b/100)+1)
+
+
+ console.log(resultado) 
+
+ document.write(resultado)
